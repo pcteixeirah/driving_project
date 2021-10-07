e917fa90478a1087706e948489948d2788e25b32
JuanCastro <jucacahi@unal.edu.co>
2021-10-06 23:42:58 -0500

    Formulario Reservar Vehiculos v2, diseño actualizado

---
+++ js/cambiarpestana.js
@@ -0,0 +1,25 @@
+	function mostrarPestana(n){
+		var pestanas = document.getElementsByClassName("pestana");
+		var cabecera = document.getElementsByClassName("cabecera");
+		for(i = 0; i<pestanas.length; i++){
+			if(pestanas[i].className.includes("p-activa")){
+				pestanas[i].className = pestanas[i].className.replace("p-activa", "");
+				cabecera[i].className = cabecera[i].className.replace("c-activa", "");
+				break;
+			}
+		}
+		pestanas[n].className += " p-activa";
+		cabecera[n].className += " c-activa";
+	}
+
+	function validate_dateI(){
+		const fecha = new Date().toISOString();
+		var fechaI = document.getElementById("diaI");
+		fechaI.setAttribute("min", fecha.substring(0, fecha.length - 8));
+	}
+
+	function validate_dateF(){
+		var fechaI = document.getElementById("diaI");
+		var fechaF = document.getElementById("diaF");
+		fechaF.setAttribute("min",fechaI.value);
+	}
